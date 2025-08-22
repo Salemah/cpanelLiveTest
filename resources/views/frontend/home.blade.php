62630008466f32f2f9fc7780d99b67ebf105120d
Salemah <kadertanvir1@gmail.com>
2025-08-22 16:26:05 +0600

    update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -28,6 +28,7 @@
                                                 <a href="{{ $team->facebook }}"><i class="fa fa-facebook fa-lg"></i></a>
                                                 <a href="{{ $team->twitter }}"><i class="fa fa-twitter fa-lg"></i></a>
                                                 <a href="{{ $team->linkedin }}"><i class="fa fa-linkedin fa-lg"></i></a>
+                                                 <a class="" title="Make Appointment" href="{{ route('frontend.team_details',['id' => $team->id]) }}"><i class="fa-regular fa-calendar-check"></i></a>
                                                 {{-- <a href="#"><i class="fa fa-pinterest fa-lg"></i></a> --}}
                                             </div>
                                         </div>
